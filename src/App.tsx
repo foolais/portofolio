5b5aa8207172bcd128c85d92d52c4405ac6296bb
Wahyu Esya <wahyu.esya17@gmail.com>
2024-08-26 15:21:52 +0700

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div className="w-screen h-screen bg-secondary flex items-center justify-center">
+      <Button>Test Btn</Button>
+    </div>
+  );
+};
+
+export default App;
