2718665009223ee280f4734706f3a4cc3668ef7e
Wahyu Esya <wahyu.esya17@gmail.com>
2024-08-29 15:47:23 +0700

    feat: change layout to scroll area

--- src/layouts/dashboard/Layout.tsx
+++ src/layouts/dashboard/Layout.tsx
@@ -2,15 +2,17 @@ import { Carrer } from "@/components/carrer";
 import { Navbar } from "@/components/navigation";
 import { Profile } from "@/components/profle";
 import { TechStack } from "@/components/techStack";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 const Layout = () => {
   return (
-    <div className="mx-auto md:flex md:11/12 pb-32">
+    <ScrollArea className="mx-auto md:flex md:11/12 h-screen">
       <Navbar />
       <Profile />
       <Carrer />
       <TechStack />
-    </div>
+      <div className="p-12" />
+    </ScrollArea>
   );
 };
 
