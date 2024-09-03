1bc9bb5cef28e5eba23bef3fcd8c3abebb0ed0ba
Wahyu Esya <wahyu.esya17@gmail.com>
2024-09-04 00:46:27 +0700

    fix: tech stack title

--- src/components/techStack/TechStack.tsx
+++ src/components/techStack/TechStack.tsx
@@ -1,13 +1,14 @@
 import { techStackData } from "@/data/data";
 import { Title } from "../title";
 import { Card, CardHeader, CardTitle } from "../ui/card";
+import { TechProps } from "@/types/types";
 
 const TechStack = () => {
   return (
     <div className="w-full px-6 mt-6">
-      <Title text="Tech Stack" />
+      <Title text="Technologies that I use" />
       <div className="flex flex-wrap items-center gap-3 my-4">
-        {techStackData.map(({ name, image }) => {
+        {techStackData.map(({ name, image }: TechProps) => {
           return (
             <Card key={name} className="w-max">
               <CardHeader className="py-2 px-4 flex-row justify-center items-center gap-2 space-y-0">
