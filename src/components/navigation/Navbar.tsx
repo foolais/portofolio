b4cbb1117c9ea98887ff35639293ce9a03083aae
Wahyu Esya <wahyu.esya17@gmail.com>
2024-09-03 20:14:35 +0700

    feat: add btn for changing theme

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -9,6 +9,9 @@ import {
 } from "../ui/tooltip";
 import { useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { Separator } from "../ui/separator";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "../theme-provider";
 
 interface Props {
   className?: string;
@@ -17,23 +20,36 @@ interface Props {
 
 const Navbar = ({ className, handleNavClick }: Props) => {
   const location = useLocation();
+  const { setTheme } = useTheme();
 
   const [currentNav, setCurrentNav] = useState<string>("");
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(true);
+
+  const handleChangeTheme = () => {
+    setIsDarkMode(!isDarkMode);
+    setTheme(isDarkMode ? "light" : "dark");
+  };
 
   useEffect(() => {
     const currentPath = location.pathname;
     const navName = currentPath === "/" ? "Home" : currentPath.split("/")[1];
-    setCurrentNav(navName);
+    setCurrentNav(navName.toLocaleLowerCase());
   }, []);
+
+  const btnIconClassName = cn(
+    "flex flex-col items-center justify-center gap-1 icon-wrapper rounded-full p-2.5",
+    "hover:scale-150 transition duration-300 ease-in-out",
+    "md:flex-row md:gap-3 md:w-full md:justify-start md:bg-transparent md:p-0 md:rounded-lg md:hover:bg-secondary md:hover:scale-100"
+  );
 
   return (
     <div
-      className={cn(className, "fixed bottom-6 left-0 right-0 md:static z-30")}
+      className={cn(className, "fixed bottom-0 left-0 right-0 md:static z-30")}
     >
       <div className="w-max mx-auto md:mx-0 md:w-full">
         <div
           className={cn(
-            "relative bg-secondary shadow gap-4 flex items-center justify-center rounded-3xl px-6 py-2",
+            "relative bg-secondary shadow gap-4 flex items-center justify-center rounded-t-2xl p-4",
             "md:bg-background md:gap-0 md:px-0 md:py-0 md:flex-col md:mt-10"
           )}
         >
@@ -43,13 +59,13 @@ const Navbar = ({ className, handleNavClick }: Props) => {
                 <TooltipTrigger asChild>
                   <Button
                     id={name}
-                    variant={currentNav === name ? "default" : "accent"}
+                    variant={
+                      currentNav === name.toLocaleLowerCase()
+                        ? "default"
+                        : "accent"
+                    }
                     size={"iconText"}
-                    className={cn(
-                      "flex flex-col items-center justify-center gap-1 icon-wrapper rounded-full p-2",
-                      "hover:scale-150 transition duration-300 ease-in-out",
-                      "md:flex-row md:gap-3 md:w-full md:justify-start md:bg-transparent md:p-0 md:rounded-lg md:hover:bg-secondary md:hover:scale-100"
-                    )}
+                    className={btnIconClassName}
                     onClick={() => handleNavClick(name)}
                   >
                     <Icon className="icon-size" />
@@ -61,6 +77,17 @@ const Navbar = ({ className, handleNavClick }: Props) => {
               </Tooltip>
             </TooltipProvider>
           ))}
+          <Separator orientation="vertical" className="h-10" />
+          <Button
+            className={`${btnIconClassName} hover:scale-100`}
+            onClick={handleChangeTheme}
+          >
+            {isDarkMode ? (
+              <Moon className="icon-size" />
+            ) : (
+              <Sun className="icon-size" />
+            )}
+          </Button>
         </div>
       </div>
     </div>
