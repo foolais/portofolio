a8eca73b959c17b668a9a0cde09a3695abc25c13
Wahyu Esya <wahyu.esya17@gmail.com>
2024-11-26 15:33:55 +0700

    fix: text color in profile desc

--- src/components/profle/ProfileDescription.tsx
+++ src/components/profle/ProfileDescription.tsx
@@ -4,15 +4,24 @@ const ProfileDescription = memo(() => {
   return (
     <div className="w-full md:w-11/12 font-semibold mt-4">
       <p className="text-sm md:text-base leading-5 tracking-wide mb-1.5">
-        A dedicated{" "}
-        <span className="text-primary font-bold">frontend developer </span>{" "}
-        based in <span className="text-primary font-bold"> Yogyakarta</span>,
-        Indonesia, with{" "}
-        <span className="text-primary font-bold">1 year of experience</span>.
+        A{" "}
+        <span className="bg-primary bg-clip-text text-transparent font-bold">
+          frontend developer{" "}
+        </span>{" "}
+        based in{" "}
+        <span className="bg-primary bg-clip-text text-transparent font-bold">
+          {" "}
+          Yogyakarta
+        </span>
+        , Indonesia, with{" "}
+        <span className="bg-primary bg-clip-text text-transparent font-bold">
+          1 year of experience
+        </span>
+        .
       </p>
       <p>
         Passionate about crafting intuitive web applications, I specialize in
-        React and Vue.js. I'm skilled at problem-solving and always eager to
+        React.js and Vue.js. I'm skilled at problem-solving and always eager to
         learn new technologies.
       </p>
     </div>
